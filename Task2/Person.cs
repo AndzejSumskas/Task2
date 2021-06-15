ebced3041ea656791e9eef908ae2ed2d10956da5
Andzej Sumskas <andzejsumskas1@gmail.com>
2021-06-15 22:12:00 +0300

    Add project files.

---
+++ Task2/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Person
+    {
+        [StringLength(20)]
+        private string name { get; set; }
+        [StringLength(20)]
+        private string surName { get; set; }
+        [MaxLength(6)]
+        private int telephonNumber { get; set; }
+
+        public Person(string name, string surName, int telephonNumber)
+        {
+            this.name = name;
+            this.surName = surName;
+            this.telephonNumber = telephonNumber;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isActive = true;
+            StringBuilder csvContent = new StringBuilder();
+            //csvContent.AppendLine("Name,Age,TelNumber");
+
+            while (isActive)
+            {
+                Console.WriteLine("Iveskite varda.");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Iveskite pavarde.");
+                string surName = Console.ReadLine();
+
+                Console.Write("Iveskite Telefono numeri.\n +370");
+                int telephonNumber = Convert.ToInt32(Console.ReadLine());
+                while(telephonNumber.ToString().Length != 8)
+                {
+                    Console.WriteLine("Klaidingas ivedimas");
+                    Console.Write("Iveskite Telefono numeri.\n +370");
+                    telephonNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                csvContent.AppendLine($"{name},{surName},+370{telephonNumber}");
+                Console.WriteLine("Ar norite prideti dar viena kontakta? N");
+                string oneMoreContact = Console.ReadLine();               
+                if(oneMoreContact == "N")
+                {
+                    isActive = false;
+                }         
+            }
+ 
+            string csvPath = @"C:\Users\Andzej\Desktop\IT Tasks\HomeWork\Task2\test.czv";
+
+            File.AppendAllText(csvPath, csvContent.ToString());
+        }
+    }
+}
